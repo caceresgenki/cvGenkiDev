49e78cd06244524d5159548b3bc3d9061ddba9a2
caceresgenki <caceresgenki@gmail.com>
2024-03-04 19:35:31 +0100

    march 04 v2

--- cvGenkiDev/src/Components/Hero.jsx
+++ cvGenkiDev/src/Components/Hero.jsx
@@ -1,7 +1,57 @@
+import React from 'react'
+import {basics} from '../cv.json'
+
+const {name, label, image, location, profiles, phone, email} = basics
+const {city, region} = location
 
 
 function Hero() {
-    return   (<h2>Información</h2>)
+    return   (
+      <>
+      <section>
+        <div className='container'>
+          <div className='info'>
+            <h1>
+              {name}
+            </h1>
+            <h2>
+              {label}
+            </h2>
+            <span>
+              {city}, {region}
+            </span>
+            <footer>
+              {
+                email && (
+                  <a
+                    href={`mailto:${email}`}
+                    title={`Enviar un correo electrónico a ${name} al correo ${email}`}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    >
+                      Email Icono
+                  </a>
+                )
+              }
+             
+            </footer>
+          </div>
+
+          <figure>
+            <img src={image} alt={name} />
+          </figure>
+
+        </div>
+
+      </section>
+      </>
+        )
    }
+
+   <style>
+
+
+   </style>
+
 
 export default Hero
