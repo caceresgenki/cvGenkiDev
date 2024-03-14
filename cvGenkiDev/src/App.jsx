7ef701e2d59b14c5d60e766ff3559cf23b47403a
caceresgenki <caceresgenki@gmail.com>
2024-03-14 21:42:42 +0100

    march 14 21.42

--- cvGenkiDev/src/App.jsx
+++ cvGenkiDev/src/App.jsx
@@ -4,8 +4,6 @@ import About from '../src/Components/About'
 import Education from '../src/Components/Education'
 import Experience from '../src/Components/Experience'
 import Hero from '../src/Components/Hero'
-import Footer from '../src/Components/Footer'
-// import Projects from '../src/Components/Projects'
 import Skills from '../src/Components/Skills'
 import KeyboardManager from '../src/Components/KeyboardManager'
 
@@ -24,7 +22,6 @@ function App() {
       <Skills />
     </main>
     <KeyboardManager />
-    <Footer />
   </>
  )
 }
--- cvGenkiDev/src/Components/KeyboardManager.jsx
+++ cvGenkiDev/src/Components/KeyboardManager.jsx
@@ -1,11 +1,11 @@
 import 'ninja-keys'
-// import { type SocialIcon } from ""
 import { basics } from "../cv.json"
+import Section from './Section'
 import LinkedIn from './Icons/LinkedIn'
 import GitHub from './Icons/GitHub'
 import '../StyleCss/KeyboardManager.css'
 
-const {name, label, image, location, profiles, phone, email} = basics
+const {profiles} = basics
 
 
 const SOCIAL_ICONS = {
@@ -32,9 +32,31 @@ const profilesInfo = profiles.map(({ network, url }) => {
 
 function KeyBoard() {
     return   (
-          <ninja-keys>
-            <h2>hola mundo</h2>
-          </ninja-keys>
+        <section className='footerKeys'>  
+          <footer id="normalFooter" className="no-print">
+            Pulsa <kbd>Cmd</kbd> ó <kbd>Ctrl</kbd>+ <kbd>K</kbd> para abrir la paleta de comandos.
+          </footer>
+          <div id="footerButton" className="no-print">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-command"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="#777"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path
+                d="M7 9a2 2 0 1 1 2 -2v10a2 2 0 1 1 -2 -2h10a2 2 0 1 1 -2 2v-10a2 2 0 1 1 2 2h-10"
+              ></path>
+            </svg>
+          </div>
+          <ninja-keys></ninja-keys>
+        </section>
         )
    }
 
