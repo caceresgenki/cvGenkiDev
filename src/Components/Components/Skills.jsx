2f539503c39379fb36f32f5a8a5ade19d84e576b
Genki Cáceres <110782088+caceresgenki@users.noreply.github.com>
2026-02-04 17:19:37 +0100

    act h4

--- src/Components/Components/Skills.jsx
+++ src/Components/Components/Skills.jsx
@@ -1,21 +1,21 @@
-import Section from './Section'
-import {skills, competencies} from '../../cv.json'
-import '../../StyleCss/Skills.css'
+import { competencies, skills } from "../../cv.json";
+import "../../StyleCss/Skills.css";
+import Section from "./Section";
 
 function Skills() {
   return (
-    <Section id='skills' title='Habilidades Técnicas'>
-      <div className='skillsContainer'>
+    <Section id="skills" title="Habilidades Técnicas">
+      <div className="skillsContainer">
         {/* Habilidades Técnicas */}
-        <div className='skillSection'>
+        <div className="skillSection">
           {/* <h3 className='skillSectionTitle'>Habilidades Técnicas</h3> */}
           {skills.map((category) => (
-            <div key={category.name} className='skillCategory'>
-              <h4 className='categoryTitle'>{category.name}:</h4>
-              <div className='ulSkills'>
+            <div key={category.name} className="skillCategory">
+              <h3 className="categoryTitle">{category.name}:</h3>
+              <div className="ulSkills">
                 {category.items.map((item) => (
                   <ul key={item}>
-                    <li className='listSkills'>
+                    <li className="listSkills">
                       <span>{item}</span>
                     </li>
                   </ul>
@@ -26,11 +26,11 @@ function Skills() {
         </div>
 
         {/* Competencias Clave */}
-        <div className='skillSection'>
-          <h3 className='skillSectionTitle'>{competencies[0].title}</h3>
-          <ul className='competenciesList'>
+        <div className="skillSection">
+          <h3 className="skillSectionTitle">{competencies[0].title}</h3>
+          <ul className="competenciesList">
             {competencies[0].items.map((competency, index) => (
-              <li key={index} className='competencyItem'>
+              <li key={index} className="competencyItem">
                 {competency}
               </li>
             ))}
@@ -38,7 +38,7 @@ function Skills() {
         </div>
       </div>
     </Section>
-  )
+  );
 }
 
-export default Skills
+export default Skills;
